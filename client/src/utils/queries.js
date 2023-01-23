7741c8a357e53cd1e7596a1c0935347c8c5f749d
BrownCAB <brown.calah@hotmail.com>
2023-01-22 22:00:42 -0800

    Added and Modified utils files

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,15 +1,22 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_ME = gql`
-query GET_ME {
-    me {
-        _id
-        username
-        email
-        bookCount
-        savedBooks
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
 
 
+        }
     }
-}
 `;
