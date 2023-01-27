9c9aca5c10854ba01a4b9aa77a2d151af001e7bb
BrownCAB <brown.calah@hotmail.com>
2023-01-26 22:33:57 -0800

    new build

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 const db = require('./config/connection');
 // add apollo server
 const { ApolloServer } = require('apollo-server-express'); 
